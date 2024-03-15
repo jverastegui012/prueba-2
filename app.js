dd8e621b1a59bd6f0fbbea6b764796d1697a4fce
jverastegui012 <163601783+jverastegui012@users.noreply.github.com>
2024-03-15 17:30:22 -0500

    examen2
    examen2

--- app.js
+++ app.js
@@ -2,9 +2,41 @@
 const GI = {
 	createElement (tag, atributes) {
 		let element = document
+        var ul=document.createElement('ol');
+        for (let index = 0; index < to_do.length; index++) {
+            const text = to_do[index];
+            var li=document.createElement('li');
+            var input=document.createElement('input');
+            var label=document.createElement('label');
+            var id='chk' + index;
+            input.id=id;
+            input.type="checkbox";
+            input.onclick = function(ele){
+                //ele.atributes['style']="text-decoration: line-through; color: gray;";
+                if(ele.target.checked){
+                    ele.target.labels[0].setAttribute('style','text-decoration: line-through; color: gray')
+                }else{
+                    ele.target.labels[0].setAttribute('style','')
+                }
+
+            };
+            input.innerText=text;
+            label.innerText=text;
+            label.htmlFor=id;
+            li.appendChild(input);
+            li.appendChild(label);
+            ul.appendChild(li);
+
+        }
+        document.querySelector("#lista").appendChild(ul);
 		return document.createElement(tag)
 	}
 }
+
+document.onload=function(){
+ GI.createElement();
+
+};
 
 
 const to_do = [
@@ -15,3 +47,4 @@ const to_do = [
     'Comprar entradas para el cine',
 ]
 
+GI.createElement();
